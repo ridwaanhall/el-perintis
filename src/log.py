d520c62debde7dbda259a9b84e7983cd5625a169
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-07-31 21:20:08 +0000

    refactor: remove unused files and streamline code structure

--- src/log.py
+++
@@ -1,5 +0,0 @@
-import time
-
-def simpan_log(ide, hasil, narasi):
-    with open("log_perintis.txt", "a") as f:
-        f.write(f"[{time.ctime()}] Ide: {ide} | Hasil: {hasil} | Narasi: {narasi}\n")
